afeeeb161c2313ac7d0becfa7192d22c8edd230e
jungs <youngsjungg@gmail.com>
2021-01-20 21:45:15 +0900

    login_activity

---
+++ app/src/main/java/com/example/sunystagram/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sunystagram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
