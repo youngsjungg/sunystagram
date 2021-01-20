3be7f6861464e319a9278424b77897f766636abf
jungs <youngsjungg@gmail.com>
2021-01-20 22:41:30 +0900

    day01

--- app/src/main/java/com/example/sunystagram/LoginActivity.kt
+++ app/src/main/java/com/example/sunystagram/LoginActivity.kt
@@ -2,11 +2,25 @@ package com.example.sunystagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-
+    var auth: FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
     }
+    fun signAndSignup(){
+        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(),
+            password_edittext.text.toString())
+            ?.addOnCompleteListener {
+                task ->
+                    if (task.isSuccessful){
+
+                    }
+            }
+    }
+
 }
