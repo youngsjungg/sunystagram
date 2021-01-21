be8b7cceddc18fbe48d2ca09e066cb04c0ec06a8
jungs <youngsjungg@gmail.com>
2021-01-21 22:15:48 +0900

    nav_tab

---
+++ app/src/main/java/com/example/sunystagram/navigation/GridFragment.kt
@@ -0,0 +1,16 @@
+package com.example.sunystagram.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainer
+import com.example.sunystagram.R
+
+class GridFragment : Fragment() {
+    override fun onCreateView (inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_grid,container,false)
+        return view
+    }
+}
